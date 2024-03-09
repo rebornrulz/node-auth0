94d8a39e424d7b90543eabe6adeffb508560e2b9
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-03-10 00:42:42 +0800

    Add files via upload

---
+++ src/management/__generated/managers/prompts-manager.ts
@@ -0,0 +1,178 @@
+import * as runtime from '../../../lib/runtime.js';
+import type { InitOverride, ApiResponse } from '../../../lib/runtime.js';
+import type {
+  PromptsSettings,
+  PromptsSettingsUpdate,
+  GetCustomTextByLanguageRequest,
+  GetPartialsRequest,
+  PutCustomTextByLanguageRequest,
+  PutPartialsRequest,
+} from '../models/index.js';
+
+const { BaseAPI } = runtime;
+
+/**
+ *
+ */
+export class PromptsManager extends BaseAPI {
+  /**
+   * Retrieve custom text for a specific prompt and language.
+   * Get custom text for a prompt
+   *
+   * @throws {RequiredError}
+   */
+  async getCustomTextByLanguage(
+    requestParameters: GetCustomTextByLanguageRequest,
+    initOverrides?: InitOverride
+  ): Promise<ApiResponse<{ [key: string]: any }>> {
+    runtime.validateRequiredRequestParams(requestParameters, ['prompt', 'language']);
+
+    const response = await this.request(
+      {
+        path: `/prompts/{prompt}/custom-text/{language}`
+          .replace('{prompt}', encodeURIComponent(String(requestParameters.prompt)))
+          .replace('{language}', encodeURIComponent(String(requestParameters.language))),
+        method: 'GET',
+      },
+      initOverrides
+    );
+
+    return runtime.JSONApiResponse.fromResponse<any>(response);
+  }
+
+  /**
+   * Get template partials for a prompt - In Early Access
+   * Get partials for a prompt
+   *
+   * @throws {RequiredError}
+   */
+  async getPartials(
+    requestParameters: GetPartialsRequest,
+    initOverrides?: InitOverride
+  ): Promise<ApiResponse<{ [key: string]: any }>> {
+    runtime.validateRequiredRequestParams(requestParameters, ['prompt']);
+
+    const response = await this.request(
+      {
+        path: `/prompts/{prompt}/partials`.replace(
+          '{prompt}',
+          encodeURIComponent(String(requestParameters.prompt))
+        ),
+        method: 'GET',
+      },
+      initOverrides
+    );
+
+    return runtime.JSONApiResponse.fromResponse<any>(response);
+  }
+
+  /**
+   * Retrieve details of the Universal Login configuration of your tenant. This includes the <a href="https://auth0.com/docs/authenticate/login/auth0-universal-login/identifier-first">Identifier First Authentication</a> and <a href="https://auth0.com/docs/secure/multi-factor-authentication/fido-authentication-with-webauthn/configure-webauthn-device-biometrics-for-mfa">WebAuthn with Device Biometrics for MFA</a> features.
+   * Get prompt settings
+   *
+   * @throws {RequiredError}
+   */
+  async get(initOverrides?: InitOverride): Promise<ApiResponse<PromptsSettings>> {
+    const response = await this.request(
+      {
+        path: `/prompts`,
+        method: 'GET',
+      },
+      initOverrides
+    );
+
+    return runtime.JSONApiResponse.fromResponse(response);
+  }
+
+  /**
+   * Update the Universal Login configuration of your tenant. This includes the <a href="https://auth0.com/docs/authenticate/login/auth0-universal-login/identifier-first">Identifier First Authentication</a> and <a href="https://auth0.com/docs/secure/multi-factor-authentication/fido-authentication-with-webauthn/configure-webauthn-device-biometrics-for-mfa">WebAuthn with Device Biometrics for MFA</a> features.
+   * Update prompt settings
+   *
+   * @throws {RequiredError}
+   */
+  async update(
+    bodyParameters: PromptsSettingsUpdate,
+    initOverrides?: InitOverride
+  ): Promise<ApiResponse<PromptsSettings>> {
+    const headerParameters: runtime.HTTPHeaders = {};
+
+    headerParameters['Content-Type'] = 'application/json';
+
+    const response = await this.request(
+      {
+        path: `/prompts`,
+        method: 'PATCH',
+        headers: headerParameters,
+        body: bodyParameters,
+      },
+      initOverrides
+    );
+
+    return runtime.JSONApiResponse.fromResponse(response);
+  }
+
+  /**
+   * Set custom text for a specific prompt. Existing texts will be overwritten.
+   * Set custom text for a specific prompt
+   *
+   * @throws {RequiredError}
+   */
+  async updateCustomTextByLanguage(
+    requestParameters: PutCustomTextByLanguageRequest,
+    bodyParameters: { [key: string]: any },
+    initOverrides?: InitOverride
+  ): Promise<ApiResponse<void>> {
+    runtime.validateRequiredRequestParams(requestParameters, ['prompt', 'language']);
+
+    const headerParameters: runtime.HTTPHeaders = {};
+
+    headerParameters['Content-Type'] = 'application/json';
+
+    const response = await this.request(
+      {
+        path: `/prompts/{prompt}/custom-text/{language}`
+          .replace('{prompt}', encodeURIComponent(String(requestParameters.prompt)))
+          .replace('{language}', encodeURIComponent(String(requestParameters.language))),
+        method: 'PUT',
+        headers: headerParameters,
+        body: bodyParameters,
+      },
+      initOverrides
+    );
+
+    return runtime.VoidApiResponse.fromResponse(response);
+  }
+
+  /**
+   * Set template partials for a prompt - In Early Access
+   * Set partials for a prompt
+   *
+   * @throws {RequiredError}
+   */
+  async updatePartials(
+    requestParameters: PutPartialsRequest,
+    bodyParameters: { [key: string]: any },
+    initOverrides?: InitOverride
+  ): Promise<ApiResponse<void>> {
+    runtime.validateRequiredRequestParams(requestParameters, ['prompt']);
+
+    const headerParameters: runtime.HTTPHeaders = {};
+
+    headerParameters['Content-Type'] = 'application/json';
+
+    const response = await this.request(
+      {
+        path: `/prompts/{prompt}/partials`.replace(
+          '{prompt}',
+          encodeURIComponent(String(requestParameters.prompt))
+        ),
+        method: 'PUT',
+        headers: headerParameters,
+        body: bodyParameters,
+      },
+      initOverrides
+    );
+
+    return runtime.VoidApiResponse.fromResponse(response);
+  }
+}
